32d99ad626a080a8cb95dc9187d99547ad62478a
Shivansh Joshi <77457296+sniperzee2@users.noreply.github.com>
2021-06-03 18:02:31 +0530

    backimg

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -23,9 +23,9 @@ module.exports = {
       editIcon: function (storyUser, loggedUser, storyId, floating = true) {
         if (storyUser._id.toString() == loggedUser._id.toString()) {
           if (floating) {
-            return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`
+            return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-user-edit"></i></a>`
           } else {
-            return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>`
+            return `<a href="/stories/edit/${storyId}"><i class="fas fa-user-edit"></i></a>`
           }
         } else {
           return ''
